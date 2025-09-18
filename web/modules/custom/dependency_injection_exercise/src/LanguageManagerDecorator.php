8ad379f64dccf0a942e79590cfd057d0dfaa5e91
Slobodan Prodanovic <slobodan.prodanovic@etondigital.com>
2025-09-18 09:56:25 +0200

    task - Refactoring complete module.

--- web/modules/custom/dependency_injection_exercise/src/LanguageManagerDecorator.php
+++ web/modules/custom/dependency_injection_exercise/src/LanguageManagerDecorator.php
@@ -9,7 +9,7 @@ use Drupal\Core\Url;
 use Psr\Log\LoggerInterface;
 
 /**
- * Simple decorator for the LanguageManager service.
+ * Decorator for the LanguageManager service.
  */
 class LanguageManagerDecorator implements LanguageManagerInterface {
 
@@ -21,31 +21,35 @@ class LanguageManagerDecorator implements LanguageManagerInterface {
   protected LanguageManagerInterface $inner;
 
   /**
-   * The logger service.
+   * Logger service.
    *
    * @var \Psr\Log\LoggerInterface
    */
   protected LoggerInterface $logger;
 
   /**
-   * The current route match service.
+   * Current route match service.
    *
    * @var \Drupal\Core\Routing\CurrentRouteMatch
    */
   protected CurrentRouteMatch $currentRoute;
 
   /**
-   * A flag to ensure logging happens only once per request.
+   * Flag to ensure logging happens only once per request.
    *
    * @var bool
    */
   protected bool $logged = FALSE;
 
   /**
-   * Constructs a LanguageManagerDecorator object.
+   * Constructs the decorator.
    *
    * @param \Drupal\Core\Language\LanguageManagerInterface $inner
    *   The decorated language manager service.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   Logger service.
+   * @param \Drupal\Core\Routing\CurrentRouteMatch $currentRoute
+   *   Current route match service.
    */
   public function __construct(LanguageManagerInterface $inner, LoggerInterface $logger, CurrentRouteMatch $currentRoute) {
     $this->inner = $inner;
@@ -54,133 +58,130 @@ class LanguageManagerDecorator implements LanguageManagerInterface {
   }
 
   /**
-   * @inheritDoc
+   * {@inheritDoc}
+   */
+  public function getCurrentLanguage($type = LanguageInterface::TYPE_INTERFACE): LanguageInterface {
+    $route = $this->currentRoute->getRouteName();
+    if ($route === 'dependency_injection_exercise.rest_output_controller_photos' && !$this->logged) {
+      $language = $this->inner->getCurrentLanguage($type);
+      $this->logger->info('Language manager is over taken. Current language: {lang} for route {route}', [
+        'lang' => $language->getId(),
+        'route' => $route,
+      ]);
+      $this->logged = TRUE;
+    }
+    return $this->inner->getCurrentLanguage($type);
+  }
+
+  /**
+   * {@inheritDoc}
    */
   public function isMultilingual(): bool {
     return $this->inner->isMultilingual();
   }
 
   /**
-   * @inheritDoc
+   * {@inheritDoc}
    */
   public function getLanguageTypes(): array {
     return $this->inner->getLanguageTypes();
   }
 
   /**
-   * @inheritDoc
+   * {@inheritDoc}
    */
   public function getDefinedLanguageTypesInfo(): array {
     return $this->inner->getDefinedLanguageTypesInfo();
   }
 
   /**
-   * @inheritDoc
+   * {@inheritDoc}
    */
-  public function getCurrentLanguage($type = LanguageInterface::TYPE_INTERFACE): LanguageInterface {
-    $route = $this->currentRoute->getRouteName();
-    if ('dependency_injection_exercise.rest_output_controller_photos' === $this->currentRoute->getRouteName() && !$this->logged) {
-      $language = $this->inner->getCurrentLanguage($type);
-      $this->logger->info('Language manager is over taken. Current language: {lang} for route {route}', [
-        'lang' => $language->getId(),
-        'route' => $route,
-      ]);
-
-      $this->logged = TRUE;
-    }
-
-    return $this->inner->getCurrentLanguage($type);
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function reset($type = NULL): LanguageManagerDecorator|LanguageManagerInterface {
+  public function reset($type = NULL): LanguageManagerInterface {
     return $this->inner->reset($type);
   }
 
   /**
-   * @inheritDoc
+   * {@inheritDoc}
    */
   public function getDefaultLanguage(): LanguageInterface {
     return $this->inner->getDefaultLanguage();
   }
 
   /**
-   * @inheritDoc
+   * {@inheritDoc}
    */
   public function getLanguages($flags = LanguageInterface::STATE_CONFIGURABLE): array {
     return $this->inner->getLanguages($flags);
   }
 
   /**
-   * @inheritDoc
+   * {@inheritDoc}
    */
   public function getNativeLanguages(): array {
     return $this->inner->getNativeLanguages();
   }
 
   /**
-   * @inheritDoc
+   * {@inheritDoc}
    */
   public function getLanguage($langcode): ?LanguageInterface {
     return $this->inner->getLanguage($langcode);
   }
 
   /**
-   * @inheritDoc
+   * {@inheritDoc}
    */
   public function getLanguageName($langcode): string {
     return $this->inner->getLanguageName($langcode);
   }
 
   /**
-   * @inheritDoc
+   * {@inheritDoc}
    */
   public function getDefaultLockedLanguages($weight = 0): array {
     return $this->inner->getDefaultLockedLanguages($weight);
   }
 
   /**
-   * @inheritDoc
+   * {@inheritDoc}
    */
   public function isLanguageLocked($langcode): bool {
     return $this->inner->isLanguageLocked($langcode);
   }
 
   /**
-   * @inheritDoc
+   * {@inheritDoc}
    */
   public function getFallbackCandidates(array $context = []): array {
     return $this->inner->getFallbackCandidates($context);
   }
 
   /**
-   * @inheritDoc
+   * {@inheritDoc}
    */
   public function getLanguageSwitchLinks($type, Url $url): ?object {
     return $this->inner->getLanguageSwitchLinks($type, $url);
   }
 
   /**
-   * @inheritDoc
+   * {@inheritDoc}
    */
-  public function setConfigOverrideLanguage(?LanguageInterface $language = NULL): LanguageManagerDecorator|LanguageManagerInterface {
+  public function setConfigOverrideLanguage(?LanguageInterface $language = NULL): LanguageManagerInterface {
     return $this->inner->setConfigOverrideLanguage($language);
   }
 
   /**
    * @inheritDoc
    */
-  public function getConfigOverrideLanguage(): LanguageInterface {
+  public function getConfigOverrideLanguage() {
     return $this->inner->getConfigOverrideLanguage();
   }
 
   /**
    * @inheritDoc
    */
-  public static function getStandardLanguageList(): array {
-    return LanguageManagerInterface::getStandardLanguageList();
+  public static function getStandardLanguageList() {
   }
 
 }
